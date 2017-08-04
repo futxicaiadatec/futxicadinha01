df50b9a5d6fb2a289828456c1b39a2e898401c81
itacir gabral <degumes@users.noreply.github.com>
2017-08-04 00:40:26 -0400

    pixel2svg

--- quinta/run.js
+++ quinta/run.js
@@ -1,58 +1,47 @@
-var pxFileList;
-var ajax = new XMLHttpRequest();
-ajax.open("GET","filelist.json",true);
-ajax.addEventListener("load", function(e){
-	pxFileList=JSON.parse(e.target.response);
-});
-ajax.send();
-/*
-hexString = yourNumber.toString(16);
-yourNumber = parseInt(hexString, 16);
-
-var pixels;
-getPixels("chrome.png",function(err,px){
-	pixels = px;
-});
-
-var pxjson = []
-for(let i=0; i<pixels.data.length/4; i++){
-	let s = pixels.data[i]+pixels.data[i+1]+pixels.data[i+2]+pixels.data[i+3];
-	  let linha = Math.trunc(i/pixels.shape[0]);
-      let coluna = i%pixels.shape[0];
-      let filename = Math.random().toString().substring(3)+".json";
-      pxjson.push({
-        i:linha,
-        j:coluna,
-        file:filename,
-		red:pixels.data[i],
-		green:pixels.data[i+1],
-		blue:pixels.data[i+2],
-		alfa:pixels.data[i+3]
-      });
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
-
-var filelist= pxjson.map(
-	({file})=>({filename:file})
-);
-
-pxjson.forEach(function(e){
-  const ajax = new XMLHttpRequest();
-  ajax.open("PUT",e.file,true);
-  ajax.send(JSON.stringify(e,null,4));
+(function url2pxarr(url){
+	return new Promise(function(resolve, reject){
+		getPixels(url,function(err,px){
+			if(err){
+				reject(Error(err));
+			}else{
+				resolve(px);
+			}
+		});
+	}).then(function(result){
+		const pxjson=[];
+		for(let i=0; i<result.data.length/4; i++){
+			let linha = Math.trunc(i/result.shape[0]);
+			let coluna = i%result.shape[0];
+			let filename = Math.random().toString().substring(3)+".json";
+			pxjson.push({
+				i:linha+1,
+				j:coluna+1,
+				file:filename,
+				red:result.data[4*i],
+				green:result.data[4*i+1],
+				blue:result.data[4*i+2],
+				alfa:result.data[4*i+3]
+			});
+		}
+		return pxjson.sort(function(a,b){
+			return a.file.localeCompare(b.file)
+		});
+	});
+})("9.png").then(function(result){
+	const svg = document.getElementsByTagName("svg")[0];
+	result.forEach(function(e){
+		c = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+		c.setAttribute("r", 10);
+		c.setAttribute("cy", e.i*30-15);
+		c.setAttribute("cx", e.j*30-15);
+		c.setAttribute("stroke-width",1);
+		c.setAttribute("stroke","black");
+		c.setAttribute("style", "fill:#"+componentToHex(e.red)+componentToHex(e.green)+componentToHex(e.blue));
+		svg.appendChild(c)
+	});
 });
-
-var a = document.createElement("a");
-document.body.appendChild(a);
-a.style = "display: none";
-blob = new Blob([JSON.stringify( _VAR_ ,null,4)], {type: "text/plain"});
-url = window.URL.createObjectURL(blob);
-a.href = url;
-a.download = "_VAR_.json";
-a.click();
-window.URL.revokeObjectURL(url);
-
-pxjson.sort(function(a,b){
-	return a.file.localeCompare(b.file)
-});
-
-*/
